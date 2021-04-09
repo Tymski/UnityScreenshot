2d2860057b73a7c21694680f8a83b104e4453ef3
Tymon Oziemblewski <Tymon.Oziemblewski@gmail.com>
2021-04-09 20:32:14 +0200

    Date formatting, icon, change directory button, assembly path

--- Editor/ScreenshotEditor.cs
+++ Editor/ScreenshotEditor.cs
@@ -1,24 +1,40 @@
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Tymski
 {
     public class Screenshot : EditorWindow
     {
-        private static string directory = "Screenshots/";
+        private const string EDITOR_PREF_KEY = "Tymski.Screenshot";
+        private const string MENU_PATH = "Tools/Tymski/Screenshots/";
+        [SerializeField, HideInInspector] private static string directory = "Screenshots/";
         private static string latestScreenshotPath = "";
         private bool initDone = false;
-
         private GUIStyle BigText;
+        private GUIStyle RichText;
+        private GUIStyle BigButton;
 
         void InitStyles()
         {
             initDone = true;
+
             BigText = new GUIStyle(GUI.skin.label)
             {
-                fontSize = 20,
+                fontSize = 18,
                 fontStyle = FontStyle.Bold
+            };
+
+            RichText = new GUIStyle(GUI.skin.label)
+            {
+                richText = true
+            };
+
+            BigButton = new GUIStyle(GUI.skin.button)
+            {
+                fixedHeight = 35,
+                fontSize = 14
             };
         }
 
@@ -29,28 +45,31 @@ namespace Tymski
                 InitStyles();
             }
 
-            GUILayout.Label("Screenshots", BigText);
-            if (GUILayout.Button("Take a screenshot"))
+            GUILayout.Label(new GUIContent("Game Screenshots", EditorGUIUtility.IconContent("unityeditor.gameview.png").image), BigText);
+            if (GUILayout.Button("Take a screenshot", BigButton))
             {
                 TakeScreenshot();
             }
-            GUILayout.Label("Resolution: " + GetResolution());
-
+            GUILayout.Label($"Resolution: <b>{GetResolution()}</b>", RichText);
+            GUILayout.Label($"Directory: <b>{directory}</b>", RichText);
             if (GUILayout.Button("Reveal in Explorer"))
             {
-                ShowFolder();
+                RevealInExplorer();
             }
-            GUILayout.Label("Directory: " + directory);
+            if (GUILayout.Button("Change Directory"))
+            {
+                ChangeDirectory();
+            }
         }
 
-        [MenuItem("Tools/Screenshots/Open Window")]
+        [MenuItem(MENU_PATH + "Open Window")]
         public static void ShowWindow()
         {
             EditorWindow.GetWindow(typeof(Screenshot));
         }
 
-        [MenuItem("Tools/Screenshots/Reveal in Explorer")]
-        private static void ShowFolder()
+        [MenuItem(MENU_PATH + "Reveal in Explorer")]
+        private static void RevealInExplorer()
         {
             if (File.Exists(latestScreenshotPath))
             {
@@ -61,16 +80,24 @@ namespace Tymski
             EditorUtility.RevealInFinder(directory);
         }
 
-        [MenuItem("Tools/Screenshots/Take a Screenshot")]
+        [MenuItem(MENU_PATH + "Take a Screenshot")]
         private static void TakeScreenshot()
         {
-            Directory.CreateDirectory(directory);
-            var currentTime = System.DateTime.Now;
-            var filename = currentTime.ToString().Replace('/', '-').Replace(':', '_') + ".png";
+            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+            var currentTime = System.DateTime.Now.ToString("yyyy-MM-dd HH'-'mm'-'ss");
+            var filename = currentTime + ".png";
             var path = directory + filename;
             ScreenCapture.CaptureScreenshot(path);
             latestScreenshotPath = path;
-            Debug.Log($"Screenshot saved: <b>{path}</b> with resolution <b>{GetResolution()}</b>");
+            Debug.Log($"Screenshot <b>{filename}</b> saved in <b>{directory}</b> with resolution <b>{GetResolution()}</b>");
+
+        }
+
+        [MenuItem(MENU_PATH + "Change save directory")]
+        private static void ChangeDirectory()
+        {
+            directory = EditorUtility.OpenFolderPanel("Save Screenshots folder", directory, "") + "/";
+            Debug.Log($"New screenshots will be saved in: {directory}");
         }
 
         private static string GetResolution()
@@ -78,6 +105,18 @@ namespace Tymski
             Vector2 size = UnityEditor.Handles.GetMainGameViewSize();
             Vector2Int sizeInt = new Vector2Int((int)size.x, (int)size.y);
             return $"{sizeInt.x}x{sizeInt.y}";
+        }
+
+        void OnEnable()
+        {
+            var data = EditorPrefs.GetString(EDITOR_PREF_KEY, JsonUtility.ToJson(this, false));
+            JsonUtility.FromJsonOverwrite(data, this);
+        }
+
+        void OnDisable()
+        {
+            var data = JsonUtility.ToJson(this, false);
+            EditorPrefs.SetString(EDITOR_PREF_KEY, data);
         }
 
     }
